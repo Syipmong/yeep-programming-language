6924399163cb9b56dfa5f45b8abf26f699ed4ffc
syipmong <syipmong5@gmail.com>
2024-01-08 16:18:27 +0100

    added a parser to convert and parse the result

--- yeep.py
+++ yeep.py
@@ -141,4 +141,84 @@ class BinOpNode:
             
 class Parser:
      
-
+        def __init__(self, tokens):
+            self.tokens = tokens
+            self.token_index = -1
+            self.advance()
+        
+        def advance(self):
+            self.token_index += 1
+            self.current_token = self.tokens[self.token_index] if self.token_index < len(self.tokens) else None
+        
+        def parse(self):
+            res = self.expr()
+            if not res.error and self.current_token.type != TT_EOF:
+                return res.failure(InvalidSyntaxError(
+                    self.current_token.pos_start, self.current_token.pos_end,
+                    "Expected '+', '-', '*' or '/'"
+                ))
+            return res
+        
+        def factor(self):
+            res = ParseResult()
+            token = self.current_token
+        
+            if token.type in (TT_PLUS, TT_MINUS):
+                res.register_advancement()
+                self.advance()
+                factor = res.register(self.factor())
+                if res.error: return res
+                return res.success(UnaryOpNode(token, factor))
+        
+            elif token.type in (TT_INT, TT_FLOAT):
+                res.register_advancement()
+                self.advance()
+                return res.success(NumberNode(token))
+        
+            elif token.type == TT_LPAREN:
+                res.register_advancement()
+                self.advance()
+                expr = res.register(self.expr())
+                if res.error: return res
+                if self.current_token.type == TT_RPAREN:
+                    res.register_advancement()
+                    self.advance()
+                    return res.success(expr)
+                else:
+                    return res.failure(InvalidSyntaxError(
+                        self.current_token.pos_start, self.current_token.pos_end,
+                        "Expected ')'"
+                    ))
+        
+            return res.failure(InvalidSyntaxError(
+                token.pos_start, token.pos_end,
+                "Expected int or float"
+            ))
+        
+        def term(self):
+            return self.bin_op(self.factor, (TT_MUL, TT_DIV))
+        
+        def expr(self):
+            return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
+        
+        def bin_op(self, func, ops):
+            res = ParseResult()
+            left = res.register(func())
+            if res.error: return res
+        
+            while self.current_token.type in ops:
+                op_token = self.current_token
+                res.register_advancement()
+                self.advance()
+                right = res.register(func())
+                if res.error: return res
+                left = BinOpNode(left, op_token, right)
+        
+            return res.success(left)
+        
+#################################################################################################
+#####   PARSE RESULT
+#####   The parse result is a data structure that contains the result of the parse.
+#####   The parse result is a data structure that contains the result of the parse.
+#################################################################################################
+        
