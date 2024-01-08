ad401da7379d93bbfd6160d3f653e2dd707cb320
syipmong <syipmong5@gmail.com>
2024-01-08 16:35:25 +0100

    catching errors and ensuring everything works

--- yeep.py
+++ yeep.py
@@ -215,12 +215,43 @@ class Parser:
                 left = BinOpNode(left, op_token, right)
         
             return res.success(left)
+        def parse_variable(self, token):
+            
         
 
 #################################################################################################
 #####   ERROR
 #####   The error class is used to handle errors.
 #####   The error class is used to handle errors.
+#################################################################################################
+        
+
+class Error:
+        
+        def __init__(self, pos_start, pos_end, error_name, details):
+            self.pos_start = pos_start
+            self.pos_end = pos_end
+            self.error_name = error_name
+            self.details = details
+        
+        def __repr__(self) -> str:
+            result = f'{self.error_name}: {self.details}\n'
+            result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+            return result
+        
+class IllegalCharError(Error):
+
+        def __init__(self, pos_start, pos_end, details):
+            super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+class InvalidSyntaxError(Error):
+
+        def __init__(self, pos_start, pos_end, details):
+            super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+
+            
+        
+
         
 
 
