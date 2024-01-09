2ec209947202d46cc3074d58d6ef0fe41807273b
syipmong <syipmong5@gmail.com>
2024-01-09 15:30:53 +0100

    Add Number class and its methods

--- yeep.py
+++ yeep.py
@@ -568,6 +568,46 @@ class ParseResult:
         if not self.error or self.advance_count == 0:
             self.error = error
         return self
+
+
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+        self.set_pos()
+
+    def set_pos(self, pos_start=None, pos_end=None):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        return self
+
+    def added_to(self, other):
+        if isinstance(other, Number):
+            return Number(self.value + other.value).set_pos(self.pos_start, other.pos_end), None
+        
+    def subtracted_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value - other.value).set_pos(self.pos_start, other.pos_end), None
+        
+    def multiplied_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value * other.value).set_pos(self.pos_start, other.pos_end), None
+        
+    def divided_by(self, other):
+        if isinstance(other, Number):
+            if other.value == 0:
+                return None, IllegalCharError(
+                    self.pos_start, other.pos_end,
+                    'Division by zero'
+                )
+            return Number(self.value / other.value).set_pos(self.pos_start, other.pos_end), None
+        
+    def __repr__(self):
+        return str(self.value)
+    
+    
+        
+
     
 
 #################################################################################################
