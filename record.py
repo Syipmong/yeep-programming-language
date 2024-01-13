5ff7d3e2c95505971b1a62a501d12c8bbb09901c
syipmong <syipmong5@gmail.com>
2024-01-13 17:56:26 +0100

    Add record.py with project_info

---
+++ record.py
@@ -0,0 +1,30 @@
+project_info = {
+    "name": "yeep.py",
+    "description": "A project for building a programming language",
+    "author": "Yipmong Said",
+    "version": "0",
+    "license": "MIT",
+    "url": " ",
+    "keywords": ["programming", "language", "compiler", "interpreter", "yeep"],
+    "classifiers": [
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Compilers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9"
+    ],
+    "install_requires": [
+        "colorama"
+    ],
+    "packages": [
+        "yeep"
+    ],
+    "scripts": [
+        "bin/yeep"
+    ],
+    "python_requires": ">=3.9"
+    
+}
+
+
+print(project_info)
